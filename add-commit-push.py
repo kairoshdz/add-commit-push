37776649f37615a17ba1b93378e7014d1818f1d2
Erick Hernandez <erickdhernandez@lewisu.edu>
2023-11-01 10:23:25 -0500

    update files

--- add-commit-push.py
+++ add-commit-push.py
@@ -1,1 +1,13 @@
-print("Add, Commit, Push")
+import os
+
+print("Add, Commit, Push\n")
+
+print("git status\n")
+print("git add -A")
+print("git commit -m 'update files'")
+print("git push")
+
+os.system("git status")
+os.system("git add -A")
+os.system("git commit -m 'update files'")
+os.system("git push")
