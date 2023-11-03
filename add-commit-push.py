44fe68fd6588ba0216a6f5be181dcb4156d6e33e
Erick Hernandez <erickdhernandez@lewisu.edu>
2023-11-03 11:57:59 -0500

    {commitMessage}

--- add-commit-push.py
+++ add-commit-push.py
@@ -1,13 +1,39 @@
 import os
-# Commented out this part because it wouldn't make sense to assign an alias each time it's run
-# os.system(g5='cd/Users/kairos8ight/Lewis/)
+import sys
+# I commented out this part because it wouldn't make sense to assign an alias each time it's run
+# os.system(echo 'alias g5="cd/Users/kairos8ight/Lewis/cpsc-20000/sprint5/add-commit-push"' >> ~/.zshrc)
+# os.system(echo 'alias acp="python3 /Users/kairos8ight/Lewis/cpsc-20000/sprint5/add-commit-push"' >> ~/.zshrc)
+# os.system('source ~/.zshrc)
+
+print("testing")
+numofArgs = len(sys.argv)
+print('Total Arguments Passed: ', numofArgs)
+if numofArgs >= 3:
+    if sys.argv[1] == "-m":
+        commitMessage = input('Enter your message: ')
+    elif sys.argv[1] == "-f":
+        print("Do you want to continue with add commit push? (y to continue):")
+        confirm = input()
+        if confirm !="y":
+            print("Canceling", confirm)
+    else:
+        commitMessage = '"Update files"'
+
+    
+if numofArgs >= 4:
+        if sys.argv [3] != '-f':
+            print("Do you want to continue with add commit push? (y to continue):")
+            confirm = input()
+        if confirm !="y":
+            print("Canceling", confirm)
+        quit()
 
 print('Add, Commit, Push\n')
 print('git status\n')
 os.system('git status')
 print('git add -A')
 os.system('git add -A')
-print('git commit -m "update files"')
-os.system('git commit -m "update files"')
+print('git commit -m + "{commitMessage}"')
+os.system('git commit -m "{commitMessage}"')
 print('git push')
 os.system('git push')
