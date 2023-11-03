40d3823e752019fbd78bc4e3106ec0918bf5b64f
Erick Hernandez <erickdhernandez@lewisu.edu>
2023-11-03 10:30:32 -0500

    update files

--- add-commit-push.py
+++ add-commit-push.py
@@ -1,15 +1,13 @@
 import os
+# Commented out this part because it wouldn't make sense to assign an alias each time it's run
+# os.system(g5='cd/Users/kairos8ight/Lewis/)
 
 print('Add, Commit, Push\n')
-
 print('git status\n')
 os.system('git status')
-
 print('git add -A')
 os.system('git add -A')
-
 print('git commit -m "update files"')
 os.system('git commit -m "update files"')
-
 print('git push')
 os.system('git push')
