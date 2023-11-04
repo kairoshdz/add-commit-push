ee9c20fd40bd2f904a571c73d5c56e7f7e9f3ea4
Erick Hernandez <erickdhernandez@lewisu.edu>
2023-11-04 00:33:24 -0500

    Update files

--- add-commit-push.py
+++ add-commit-push.py
@@ -24,7 +24,7 @@ numofArgs = len(sys.argv)
 print('Total Arguments Passed: ', numofArgs)
 
 print('git status\n')
-os.system(f'{green} git status{reset}')
+os.system(f'{green}git status{reset}')
 
 def acp():
     print(f'{magenta}Add, Commit, Push\n {reset}')
