8b00987f0dd42725836d8bfe55bccf58ee707da6
Erick Hernandez <erickdhernandez@lewisu.edu>
2023-11-03 23:50:39 -0500

    Update files

--- add-commit-push.py
+++ add-commit-push.py
@@ -1,3 +1,8 @@
+#Erick Hernandez
+#Add Commit Push
+#Intro To Computer Science
+#Credit to Eric Pogue for helping on getting started
+
 import os
 import sys
 # I commented out this part because it wouldn't make sense to assign an alias each time it's run
@@ -5,18 +10,20 @@ import sys
 # os.system(echo 'alias acp="python3 /Users/kairos8ight/Lewis/cpsc-20000/sprint5/add-commit-push"' >> ~/.zshrc)
 # os.system('source ~/.zshrc)
 
+#default value for commit message
 commitMessage = '"Update files"'
 
 numofArgs = len(sys.argv)
 print('Total Arguments Passed: ', numofArgs)
 
+print('git status\n')
+os.system('git status')
+
 def acp():
     print('Add, Commit, Push\n')
-    print('git status\n')
-    os.system('git status')
     print('git add -A')
     os.system('git add -A')
-    print('git commit -m' + commitMessage)
+    print('git commit -m ' + commitMessage)
     os.system('git commit -m' + commitMessage)
     print('git push')
     os.system('git push')
